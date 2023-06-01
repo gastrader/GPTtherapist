994406431742dd5b3fcf1ae89e01fb751216cf35
gastraderr <gaslimits@gmail.com>
2023-05-31 22:56:33 -0400

    videos being addeed to s3 buckets

--- src/server/api/routers/generate.ts
+++ src/server/api/routers/generate.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
@@ -9,7 +10,7 @@ import {createTRPCRouter,protectedProcedure} from "~/server/api/trpc";
 import  { Configuration, OpenAIApi } from "openai";
 import { env } from "~/env.mjs";
 import axios from "axios";
-// import AWS from "aws-sdk";
+import AWS from "aws-sdk";
 
 
 const configuration = new Configuration({
@@ -17,13 +18,13 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-// const s3 = new AWS.S3({
-//     credentials: {
-//         accessKeyId: env.ACCESS_KEY_ID,
-//         secretAccessKey: env.SECRET_ACCESS_KEY,
-//     },
-//     region: "us-east-1",
-// })
+const s3 = new AWS.S3({
+    credentials: {
+        accessKeyId: env.ACCESS_KEY_ID,
+        secretAccessKey: env.SECRET_ACCESS_KEY,
+    },
+    
+})
 
 export const generateRouter = createTRPCRouter({
     //mutation = insert/delete or modify state of server
@@ -93,7 +94,7 @@ export const generateRouter = createTRPCRouter({
         const res = await axios.request(post_options)
 
         const clipId: string = res.data.id
-        // const clipId = "clp_yhWAiteFPzdYrLwCc58Zi"
+        // const clipId = "clp_vWmcYyrNSUZaFxBNJ2Zxv"
         console.log("This clip id is: ", clipId)
 
         const get_options = {
@@ -128,21 +129,44 @@ export const generateRouter = createTRPCRouter({
 
         //TO DO: convert mp4 to BLOB and store in bucket.
 
+
+
+        // const resultUrl = "https://d-id-clips-prod.s3.us-west-2.amazonaws.com/google-oauth2%7C102007001941522101997/clp_vWmcYyrNSUZaFxBNJ2Zxv/amy-jcwCkr1grs.mp4?AWSAccessKeyId=AKIA5CUMPJBIJ7CPKJNP&Expires=1685653651&Signature=sgflUYsL7xavswHmGLmbPKPILMM%3D&X-Amzn-Trace-Id=Root%3D1-6477b713-7de7a8f039537d66031eb286%3BParent%3D0852d48371ffdb93%3BSampled%3D0%3BLineage%3D84e41ec0%3A0"
         
+        async function convertmp4tobase64(url: string) {
+            try {
+                const response = await axios.get(url, {
+                    responseType: 'arraybuffer',
+                });
+                const base64 = Buffer.from(response.data, 'binary').toString('base64');
+                return base64
+            } catch (error) {
+                throw error;
+            }
+        }
+
+        convertmp4tobase64(resultUrl)
+            .then(base64Data => {
+            const params = {
+                Bucket: "gpttherapy",
+                Body: Buffer.from(base64Data, "base64"),
+                Key: convo.id,
+                ContentEncoding: "base64",
+                ContentType: "video/mp4",
+            };
+            s3.putObject(params, (err, data) => {
+                if (err) {
+                    console.error(err)
+                } else {
+                    console.log("base64 logged here... should be added to bucket as well: ", data);
+
+                }
+            })
+            
+        }).catch(error => {
+            console.error(error);
+        })
         
-        // const resulturl = "https://d-id-clips-prod.s3.us-west-2.amazonaws.com/google-oauth2%7C102007001941522101997/clp_yhWAiteFPzdYrLwCc58Zi/amy-jcwCkr1grs.mp4?AWSAccessKeyId=AKIA5CUMPJBIJ7CPKJNP&Expires=1685562716&Signature=Xsm77wcU21Tu2xWxRAyK200TGeo%3D&X-Amzn-Trace-Id=Root%3D1-647653dc-2189d0777cbaa9e13505033c%3BParent%3D419cc07554a6cb83%3BSampled%3D0%3BLineage%3D84e41ec0%3A0"
-        
-        // axios.get(resulturl, {responseType: "blob"}).then(function(response) {
-        //     const blob = response.data;
-        //     s3.putObject({
-        //         Bucket: "gpttherapy",
-        //         Key: convo.id,
-        //         Body: blob,
-        //     })
-        //     console.log("Blob object has been created");
-        // }).catch(function(error) {
-        //     console.log('Error:', error);
-        // })
 
         return{
             aiMessage: resultUrl
