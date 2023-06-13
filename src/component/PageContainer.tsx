5f614e79e0e5bd869e2630d477f9ab44645695c2
kysley <evan@kysley.com>
2023-06-13 16:40:19 -0400

    add title and description to PageContainer

--- src/component/PageContainer.tsx
+++ src/component/PageContainer.tsx
@@ -2,7 +2,19 @@ import { type ReactNode } from "react";
 
 type PageContainerProps = {
   children: ReactNode;
+  title: string;
+  description?: string;
 };
-export const PageContainer = ({ children }: PageContainerProps) => (
-  <div className="flex-grow space-y-6 p-10 pb-16">{children}</div>
+export const PageContainer = ({
+  children,
+  title,
+  description,
+}: PageContainerProps) => (
+  <div className="flex-grow space-y-6 p-10 pb-16">
+    <div className="space-y-0.5">
+      <h2 className="text-2xl font-bold tracking-tight">{title}</h2>
+      <p className="pb-2 text-muted-foreground">{description}</p>
+    </div>
+    {children}
+  </div>
 );
--- src/component/profile/settingsLayout.tsx
+++ src/component/profile/settingsLayout.tsx
@@ -23,13 +23,10 @@ interface SettingsLayoutProps {
 
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
   return (
-    <PageContainer>
-      <div className="space-y-0.5">
-        <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
-        <p className="pb-2 text-muted-foreground">
-          Manage your account settings and set e-mail preferences.
-        </p>
-      </div>
+    <PageContainer
+      title="Settings"
+      description="Manage your account settings and set e-mail preferences."
+    >
       <Separator className="my-6" />
       <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
         <aside className="-mx-4 lg:w-1/5">
