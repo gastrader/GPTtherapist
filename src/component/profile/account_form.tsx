83561a1d2125eb3610c35ea58402ffcb32f4ccc3
kysley <evan@kysley.com>
2023-06-13 14:46:23 -0400

    fix data flow with query on submit

--- src/component/profile/account_form.tsx
+++ src/component/profile/account_form.tsx
@@ -1,152 +1,146 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-misused-promises */
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
-import { Button } from "../ui/button"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Button } from "../ui/button";
 
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "../ui/select"
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-} from "../ui/popover"
-import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage
-} from "../ui/form"
-import { cn } from "../../lib/utils"
-import { Calendar } from "../ui/calendar"
-import { format } from "date-fns"
-import { CalendarIcon } from "lucide-react"
-import { api } from "~/utils/api"
-import { useState } from "react"
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { cn } from "../../lib/utils";
+import { Calendar } from "../ui/calendar";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { api } from "~/utils/api";
+import { useState } from "react";
 
 const accountFormSchema = z.object({
-    type: z
-        .string({
-            required_error: "Please select a type to retrieve.",
-        }),
-    date: z.date({
-        required_error: "Please select a date to retrieve the transcript.",
-    }),
-})
+  type: z.string({
+    required_error: "Please select a type to retrieve.",
+  }),
+  date: z.date({
+    required_error: "Please select a date to retrieve the transcript.",
+  }),
+});
 
-type AccountFormValues = z.infer<typeof accountFormSchema>
+type AccountFormValues = z.infer<typeof accountFormSchema>;
 
 export function AccountForm() {
+  const [submittedValues, setSubmittedValues] = useState<
+    AccountFormValues | undefined
+  >();
 
-    const [response, setResponse] = useState({});
-    
-    const form = useForm<AccountFormValues>({
-        resolver: zodResolver(accountFormSchema),
-    }) 
-    
-    const chats = api.chatqueryRouter.queryChat.useMutation()    
+  const form = useForm<AccountFormValues>({
+    resolver: zodResolver(accountFormSchema),
+  });
 
-    function onSubmit(data: AccountFormValues) {
-        console.log("we are in submit function", data);
-        //intentionally using mutate instead of query so that it can be called inside "onSubmit"
-        chats.mutate({ type: data.type, date: data.date }, {
-            onSuccess: (response) => {
-                console.log("The chats mutation result is:", response);
-                setResponse(response)
-            },
-            onError: (error) => {
-                console.error("Error occurred during mutation:", error); 
-            },
-        });
+  const { data, isLoading } = api.chatqueryRouter.queryChat.useQuery(
+    // @ts-expect-error The query isn't enabled if submittedValues is undefined, so the query input should always exist
+    submittedValues,
+    {
+      enabled: typeof submittedValues !== "undefined",
+      queryKey: ["transcripts", submittedValues?.date, submittedValues?.type],
     }
+  );
 
-    return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 pt-6">
-                <FormField
-                    control={form.control}
-                    name="type"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Type</FormLabel>
-                            <Select onValueChange={field.onChange} defaultValue={field.value}>
-                                <FormControl>
-                                    <SelectTrigger>
-                                        <SelectValue placeholder="Select the type of transcript to retrieve" />
-                                    </SelectTrigger>
-                                </FormControl>
-                                <SelectContent>
-                                    <SelectItem value="chat">Chat Logs</SelectItem>
-                                    <SelectItem value="video">Video Transcript</SelectItem>
-                                </SelectContent>
-                            </Select>
-                            <FormDescription>
-                                Do you want to retrieve a video transcript or chat transcript?
-                            </FormDescription>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="date"
-                    render={({ field }) => (
-                        <FormItem className="flex flex-col">
-                            <FormLabel>Date of birth</FormLabel>
-                            <Popover>
-                                <PopoverTrigger asChild>
-                                    <FormControl>
-                                        <Button
-                                            variant={"outline"}
-                                            className={cn(
-                                                "w-[240px] pl-3 text-left font-normal",
-                                                !field.value && "text-muted-foreground"
-                                            )}
-                                        >
-                                            {field.value ? (
-                                                format(field.value, "PPP")
-                                            ) : (
-                                                <span>Pick a date</span>
-                                            )}
-                                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                                        </Button>
-                                    </FormControl>
-                                </PopoverTrigger>
-                                <PopoverContent className="w-auto p-0" align="start">
-                                    <Calendar
-                                        mode="single"
-                                        selected={field.value}
-                                        onSelect={field.onChange}
-                                        disabled={(date) =>
-                                            date > new Date() || date < new Date("1900-01-01")
-                                        }
-                                        initialFocus
-                                    />
-                                </PopoverContent>
-                            </Popover>
-                            <FormDescription>
-                                The date of the transcript to retrieve.
-                            </FormDescription>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit">Get Transcript</Button>
-            </form>
-        </Form>
-    )
+  console.log({ data, isLoading });
+
+  function onSubmit(data: AccountFormValues) {
+    console.log("we are in submit function", data);
+    setSubmittedValues(data);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit, console.log)}
+        className="space-y-8 pt-6"
+      >
+        <FormField
+          control={form.control}
+          name="type"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Type</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select the type of transcript to retrieve" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="chat">Chat Logs</SelectItem>
+                  <SelectItem value="video">Video Transcript</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormDescription>
+                Do you want to retrieve a video transcript or chat transcript?
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="date"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>Date of birth</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[240px] pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    disabled={(date) =>
+                      date > new Date() || date < new Date("1900-01-01")
+                    }
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormDescription>
+                The date of the transcript to retrieve.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Get Transcript</Button>
+      </form>
+    </Form>
+  );
 }
--- src/server/api/routers/chatQuery.ts
+++ src/server/api/routers/chatQuery.ts
@@ -3,57 +3,62 @@ import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { z } from "zod";
 
 export const chatqueryRouter = createTRPCRouter({
-    //mutation = insert/delete or modify state of server
-    //query = get data back
-    queryChat: protectedProcedure.input(
-        z.object({
-            type: z.string(),
-            date: z.date(),
-            
-    //intentionally using mutate instead of query so that it can be called inside "onSubmit"
-    })).mutation(async ({ ctx, input }) => {
-        let selectedFields = {};
-        
-        if (input.type == "chat") {
-            selectedFields = {
-                text_ai_response:true,
-                text_prompt: true,
-            }
-        } else if (input.type == "video"){
-            selectedFields = {
-                video_ai_response:true,
-                video_prompt:true,
-            }
-        }
-        
-        const oldDate = new Date(input.date)
-        const dbDate = oldDate.toISOString();
-        const nextDayDate = new Date(oldDate.setUTCDate(oldDate.getUTCDate() + 1));
-        const nextDate = nextDayDate.toISOString();
-        
-        console.log("The updated date is: ",dbDate);
-        console.log("the next date is: ", nextDate);
-        let chats = await ctx.prisma.message.findMany({
-            where: {
-                userId: ctx.session.user.id,
-                createdAt: {
-                    gte: dbDate,
-                    lt: nextDate ,
-                }
-            },
-            select: selectedFields,
+  //mutation = insert/delete or modify state of server
+  //query = get data back
+  queryChat: protectedProcedure
+    .input(
+      z.object({
+        type: z.string(),
+        date: z.date(),
+
+        //intentionally using mutate instead of query so that it can be called inside "onSubmit"
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      let selectedFields = {};
+
+      if (input.type == "chat") {
+        selectedFields = {
+          text_ai_response: true,
+          text_prompt: true,
+        };
+      } else if (input.type == "video") {
+        selectedFields = {
+          video_ai_response: true,
+          video_prompt: true,
+        };
+      }
+
+      const oldDate = new Date(input.date);
+      const dbDate = oldDate.toISOString();
+      const nextDayDate = new Date(
+        oldDate.setUTCDate(oldDate.getUTCDate() + 1)
+      );
+      const nextDate = nextDayDate.toISOString();
+
+      console.log("The updated date is: ", dbDate);
+      console.log("the next date is: ", nextDate);
+      let chats = await ctx.prisma.message.findMany({
+        where: {
+          userId: ctx.session.user.id,
+          createdAt: {
+            gte: dbDate,
+            lt: nextDate,
+          },
+        },
+        select: selectedFields,
+      });
+      chats = chats.map((chat) => {
+        const chatAsIndexable = chat as { [key: string]: any };
+        Object.keys(chatAsIndexable).forEach((key) => {
+          if (chatAsIndexable[key] === null) {
+            delete chatAsIndexable[key];
+          }
         });
-        chats = chats.map((chat) => {
-            const chatAsIndexable = chat as { [key: string]: any };
-            Object.keys(chatAsIndexable).forEach((key) => {
-                if (chatAsIndexable[key] === null) {
-                    delete chatAsIndexable[key];
-                }
-            });
-            return chatAsIndexable;
-        });
-        chats = chats.filter((chat) => Object.keys(chat).length > 0)
-        console.log(chats)
-        return chats;
+        return chatAsIndexable;
+      });
+      chats = chats.filter((chat) => Object.keys(chat).length > 0);
+      console.log(chats);
+      return chats;
     }),
 });
