9198fa220d06fb9fca2597d7b229da3268cb2e47
kysley <evan@kysley.com>
2023-06-13 23:23:23 -0400

    support updating a conversation

--- src/server/api/routers/conversation.ts
+++ src/server/api/routers/conversation.ts
@@ -106,4 +106,97 @@ export const conversationRouter = createTRPCRouter({
         console.log(e);
       }
     }),
+  updateConversation: protectedProcedure
+    .input(
+      z.object({
+        message: z.string(),
+        conversationId: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      if (!ctx.session.user.id) return;
+
+      const { session } = ctx;
+
+      const user = await ctx.prisma.user.findUnique({
+        where: {
+          id: session.user.id,
+        },
+      });
+
+      if (!user) return;
+
+      if (user.credits < CONVERSATION_REPLY_CREDITS) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: `You do not have enough credits. Needed: `,
+        });
+      }
+
+      try {
+        const conversation = await ctx.prisma.conversation.findUnique({
+          where: {
+            id: input.conversationId,
+          },
+        });
+
+        if (!conversation) throw new TRPCError({ code: "BAD_REQUEST" });
+
+        if (conversation.mode === "VIDEO") {
+          const { aiResponse, base64, clipId, resultUrl } =
+            await createAIVideoResponse(input.message);
+
+          const message = await ctx.prisma.message.create({
+            data: {
+              prompt: input.message,
+              aiResponseText: aiResponse,
+              aiResponseUrl: resultUrl, // this should be the bucket url ideally
+              user: {
+                connect: {
+                  id: session.user.id,
+                },
+              },
+              conversation: {
+                connect: {
+                  id: conversation.id,
+                },
+              },
+            },
+            include: {
+              conversation: true,
+            },
+          });
+
+          const bucketKey = `${session.user.id}-${conversation.id}-${message.id}`;
+
+          s3PutBase64(base64, bucketKey);
+
+          return message;
+        }
+
+        const aiResponse = await getChatResponse(input.message);
+        const message = await ctx.prisma.message.create({
+          data: {
+            prompt: input.message,
+            aiResponseText: aiResponse,
+            user: {
+              connect: {
+                id: session.user.id,
+              },
+            },
+            conversation: {
+              connect: {
+                id: conversation.id,
+              },
+            },
+          },
+          include: {
+            conversation: true,
+          },
+        });
+        return message;
+      } catch (e) {
+        console.log(e);
+      }
+    }),
 });
